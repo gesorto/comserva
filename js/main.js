cc1d856948aaebb14066fac73a9c58f28d30aeab
Gerardo Sorto <gerardo.sorto31@outlook.com>
2020-03-26 17:26:40 -0600

    Uploading files

---
+++ js/main.js
@@ -0,0 +1,13 @@
+//Smooth Scrolling
+$('header a, .portada-btn').on('click', function(event){
+    if(this.hash !== ''){
+      event.preventDefault();
+      const hash = this.hash;
+      $('html, body').animate(
+        {
+        scrollTop: $(hash).offset().top - 100
+      },
+      800
+      );
+    }
+  });
